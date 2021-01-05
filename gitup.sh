4f15fb8adeae4f34c5ffea8ea0cc1b9e3bf02171
Kentaro Miuchi <miuchi@panda.kobe-u.ac.jp>
2021-01-05 16:49:58 +0900

    initialize yy

--- gitup.sh
+++ gitup.sh
@@ -1,4 +1,7 @@
+git remote set-url origin https://github.com/kobeDM/newage-slowmonitor-viewer
+
 git branch 4.9.1
 git checkout 4.9.1
 git commit -m "change the working directory"
 git push origin 4.9.1
+
