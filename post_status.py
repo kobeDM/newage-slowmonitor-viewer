259b5eba665d7a93dc38ef2105163d1b39e1efe5
Satoshi Higashino <higashino@people.kobe-u.ac.jp>
2022-05-11 09:21:54 +0900

    poststatus.py updated

--- post_status.py
+++ post_status.py
@@ -1,10 +1,13 @@
-#! /usr/local/bin/python3
+#! /usr/bin/python3
 import subprocess
-file="/home/msgc/slow_monitor/newage-slowmonitor-viewer/token_202112"
+#file="/home/msgc/slow_monitor_local/newage-slowmonitor-viewer/token_202112"
+#file="/home/msgc/slow_monitor_local/newage-slowmonitor-viewer/token_202203"
+file="/home/msgc/slow_monitor/newage-slowmonitor-viewer/token_202203"
 with open(file)as f:
     token=f.read()
 #token= '"'"Authorization: Bearer "+ token+'"'
 #print(token)
+#com="curl -F file=@/home/msgc/status_local/status_up.png -F "+'"'+"initial_comment=NEWAGE 0.3b status :smile:"+'"'+" -F channels=C02CS4T4SH2 -H"+ '"'+"Authorization: Bearer "+ token+'"'+" https://slack.com/api/files.upload"
 com="curl -F file=@/home/msgc/status_tmp/status_up.png -F "+'"'+"initial_comment=NEWAGE 0.3b status :smile:"+'"'+" -F channels=C02CS4T4SH2 -H"+ '"'+"Authorization: Bearer "+ token+'"'+" https://slack.com/api/files.upload"
 print(com)
 subprocess.run(com,shell=True)
